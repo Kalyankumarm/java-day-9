90ff1233db4aea2d5297b5eafd311140d3e34906
Kalyankumarm <113247412+Kalyankumarm@users.noreply.github.com>
2023-07-20 14:38:33 +0530

    Create program4.java

---
+++ program4.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class program 
+{
+    
+  static void PrintReverseOrder(int N)
+  {
+      for (int i = N; i > 0; i--)
+		System.out.print( +i + " ");
+  }
+  public static void main(String[] args)
+  {
+	int N = 100;
+
+	PrintReverseOrder(N);
+      
+  }
+}
